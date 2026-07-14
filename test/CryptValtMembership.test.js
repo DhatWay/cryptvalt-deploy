74a98cbbe3565fb325307b798a7b214672b3fa3c
iamthevgod <dhatway2024@gmail.com>
2026-07-14 18:13:55 -0400

    Update CryptValtMembership.test.js

--- test/CryptValtMembership.test.js
+++ test/CryptValtMembership.test.js
@@ -6,9 +6,9 @@ describe("CryptValtMembership", function () {
     const [owner, addr1] = await ethers.getSigners();
     const Membership = await ethers.getContractFactory("CryptValtMembership");
     const membership = await Membership.deploy(owner.address);
-    await membership.deployed();
+    await membership.waitForDeployment();
 
-    await membership.connect(addr1).mintBronze({ value: ethers.utils.parseEther("0.05") });
+    await membership.connect(addr1).mintBronze({ value: ethers.parseEther("0.05") });
 
     expect(await membership.balanceOf(addr1.address)).to.equal(1);
     expect(await membership.tierOf(1)).to.equal(1);
