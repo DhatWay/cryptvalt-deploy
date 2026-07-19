b6d184f7711b671287c6dc578e3a3e8f5198b414
iamthevgod <dhatway2024@gmail.com>
2026-07-19 00:16:27 -0400

    Add files via upload

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,12 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: "0.8.24",
-    settings: {
-      optimizer: { enabled: true, runs: 200 },
-    },
+  version: "0.8.28",
+  settings: {
+    optimizer: { enabled: true, runs: 200 },
+    evmVersion: "cancun",
   },
+},
   networks: {
     hardhat: {},
     sepolia: {
