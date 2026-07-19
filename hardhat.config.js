dd2059623b111050cf69f05f5fd43efddc566ee7
iamthevgod <dhatway2024@gmail.com>
2026-07-19 13:41:15 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("solidity-coverage");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  ssolidity: {
-  version: "0.8.28",
-  settings: {
-    optimizer: { enabled: true, runs: 200 },
-    evmVersion: "cancun",
-    viaIR: true,
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: { enabled: true, runs: 200 },
+      evmVersion: "cancun",
+      viaIR: true,
+    },
   },
-},
   networks: {
     hardhat: {},
     sepolia: {
